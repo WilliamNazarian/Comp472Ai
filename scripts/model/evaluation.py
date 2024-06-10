31fd441213c13c17e27c223b20a9349ef5e9161e
Bryan <miguelbryancarlo3434@gmail.com>
2024-06-09 21:59:51 -0400

    Calculates confusion matrix stats after evaluation

--- scripts/model/evaluation.py
+++ scripts/model/evaluation.py
@@ -10,19 +10,19 @@ import numpy as np
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader, random_split
+from typing import List, Tuple
+from scripts.model.types import EvaluationResults
 
 
 __device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def evaluate_model(model: nn.Module, dataloader: DataLoader):
+def evaluate_model(model: nn.Module, dataloader: DataLoader) -> EvaluationResults:
     model.eval().to(__device)
 
-    # Initialize variables to track correct predictions and total samples
-    correct = 0
-    total = 0
+    expected_vs_actual_pairs: List[Tuple[int, int]] = []
 
-    # Disable gradient computation for evaluation
+    # evaluation/testing
     with torch.no_grad():
         for images, labels in dataloader:
             images, labels = images.to(__device), labels.to(__device)
@@ -31,11 +31,14 @@ def evaluate_model(model: nn.Module, dataloader: DataLoader):
             outputs = model(images)
             # Get predictions from the maximum value
             _, predicted = torch.max(outputs.data, 1)
-            # Update total number of labels
-            total += labels.size(0)
-            # Update the number of correct predictions
-            correct += (predicted == labels).sum().item()
 
-    # Calculate and print the accuracy
-    accuracy = (correct / total) * 100
-    print('Test Accuracy of the model on  test images: {:.2f} %'.format(accuracy))
+            expected = labels.tolist()
+            actual = predicted.tolist()
+            expected_vs_actual_pairs.extend(list(zip(expected, actual)))
+
+    # generate confusion matrix
+    confusion_matrix = np.zeros((4, 4), dtype=int)
+    for expected, actual in expected_vs_actual_pairs:
+        confusion_matrix[expected, actual] += 1
+
+    return EvaluationResults(raw_tuples=expected_vs_actual_pairs, confusion_matrix=confusion_matrix)
--- scripts/model/types.py
+++ scripts/model/types.py
@@ -1,7 +1,9 @@
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
+import numpy as np
+import numpy.typing as npt
 
+from torch.utils.data import DataLoader
 from typing import List, Tuple
 from dataclasses import dataclass
 
@@ -23,10 +25,41 @@ class TrainingConfig:
     optimizer: optim.Optimizer
 
 
-@dataclass
+@dataclass(frozen=True)  # 'frozen=True' means type is immutable
 class EvaluationResults:
-    # List of tuples (a, b), where 'a' represents the PREDICTED class, while 'b' represents the ACTUAL class
-    true_positives: List[Tuple[int, int]]
-    false_positives: List[Tuple[int, int]]
-    true_negatives: List[Tuple[int, int]]
-    false_negatives: List[Tuple[int, int]]
+    raw_tuples: List[Tuple[int, int]]  # List of tuples (a, b), where 'a' represents the PREDICTED class, while 'b' represents the ACTUAL class
+    confusion_matrix: npt.NDArray[int]
+
+    def total_tests(self):
+        return len(self.raw_tuples)
+
+    def calculate_metrics(self):
+        true_positives = np.diag(self.confusion_matrix)
+        false_positives = np.sum(self.confusion_matrix, axis=0) - true_positives
+        false_negatives = np.sum(self.confusion_matrix, axis=1) - true_positives
+        true_negatives = np.sum(self.confusion_matrix) - (true_positives + false_positives + false_negatives)
+
+        return true_positives, false_positives, true_negatives, false_negatives
+
+    def calculate_precision(self):
+        true_positives, false_positives, _, _ = self.calculate_metrics()
+        true_positives = np.sum(true_positives)
+        false_positives = np.sum(false_positives)
+
+        return true_positives / (true_positives + false_positives)
+
+    def calculate_recall(self):
+        true_positives, _, _, false_negatives = self.calculate_metrics()
+        true_positives = np.sum(true_positives)
+        false_negatives = np.sum(false_negatives)
+
+        return true_positives / (true_positives + false_negatives)
+
+    def calculate_accuracy(self):
+        true_positives, false_positives, true_negatives, false_negatives = self.calculate_metrics()
+        true_positives = np.sum(true_positives)
+        false_positives = np.sum(false_positives)
+        true_negatives = np.sum(true_negatives)
+        false_negatives = np.sum(false_negatives)
+
+        return (true_positives + true_negatives) / (true_positives + false_positives + true_negatives + false_negatives)
