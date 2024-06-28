330e5faf91c8522be23bbbc9a8ef7b7184c2b39f
Bryan <miguelbryancarlo3434@gmail.com>
2024-06-27 22:52:31 -0400

    k-fold implementation revert I

--- src/kfold/kfold_training_testing.py
+++ src/kfold/kfold_training_testing.py
@@ -2,41 +2,92 @@ import os
 import torch
 import pickle
 import numpy as np
+import pipe as pipe
 import src.training as training
 import src.evaluation as evaluation
-import src.data_loader as data_loader
-import src.kfold.splitting_strategies.manual as manual_split
-import src.kfold.splitting_strategies.scikit_learn as scikit_split
 
 from typing import List, Tuple
+from dataclasses import dataclass
+from torch.utils.data import Subset
 from src.types import TrainingLogger, EvaluationResults
+from src.data_loader import create_data_loader
 from src.kfold.kfold_training_config import KFoldTrainingConfig
 from src.models.main_model import OB_05Model
 
 
+@dataclass
+class SubsetIndices:
+    """
+    Type containing the indices for the training, validation, and testing partitions of the dataset.
+    """
+    training_indices: List[int]
+    validation_indices: List[int]
+    testing_indices: List[int]
+
+    @staticmethod
+    def to_data_loaders(image_folder_dataset, subset_indices):
+        training_subset = Subset(image_folder_dataset, subset_indices.training_indices)
+        validation_subset = Subset(image_folder_dataset, subset_indices.validation_indices)
+        testing_subset = Subset(image_folder_dataset, subset_indices.testing_indices)
+        return create_data_loader(training_subset), create_data_loader(validation_subset), create_data_loader(
+            testing_subset)
+
+
+def __start_end_ratios_to_indices(image_folder_dataset, start_ratio, end_ratio):
+    """
+    Helper method converting a tuple of ratios in the form of (x, y) into the corresponding indices in the dataset,
+    where x and y are between 0 and 1 inclusive.
+    """
+    dataset_size = len(image_folder_dataset)
+    start_index = int(dataset_size * start_ratio)
+    end_index = int(dataset_size * end_ratio)
+    return list(range(start_index, end_index))
+
+
+def __get_subset_indices_per_fold(image_folder_dataset, num_folds):
+    """
+    Returns a list of `SubsetIndices` objects based on the number of folds.
+    """
+    subset_length_ratio = 1 / num_folds
+
+    fold_start_end_ratios = list([x for x in range(num_folds)]
+                                 | pipe.map(lambda i: i * subset_length_ratio)
+                                 | pipe.map(lambda x: (x, x + subset_length_ratio)))
+
+    all_indices = set(range(len(image_folder_dataset)))
+
+    f = __start_end_ratios_to_indices
+    return (
+        list([x for x in range(num_folds)]
+             | pipe.map(lambda i: (fold_start_end_ratios[i], fold_start_end_ratios[i - 1]))
+             | pipe.map(lambda pair: (f(image_folder_dataset, pair[0][0], pair[0][1]), f(image_folder_dataset, pair[1][0], pair[1][1])))
+             | pipe.map(lambda pair: (pair[0], pair[1], list(all_indices - (set(pair[0]) | set(pair[1])))))
+             | pipe.map(lambda ntuple: SubsetIndices(testing_indices=ntuple[0], validation_indices=ntuple[1], training_indices=ntuple[2]))
+             ))
+
+
 def kfold_cross_validation(training_config: KFoldTrainingConfig) -> List[Tuple[TrainingLogger, EvaluationResults]]:
-    num_folds = training_config.num_folds
     dataset = training_config.dataset
-    dataset_subsets_per_fold = scikit_split.get_dataset_subsets_per_fold(dataset, num_folds)
+    num_folds = training_config.num_folds
+    subset_indices_list = __get_subset_indices_per_fold(dataset, num_folds)
 
     results_per_fold: List[Tuple[TrainingLogger, EvaluationResults]] = []
 
-    for current_index, (training_subset, validation_subset, testing_subset) in enumerate(dataset_subsets_per_fold):
-        # 1. setting up output folders and initial values
+    for current_index, subset_indices in enumerate(subset_indices_list):
+        # 1. setups
         fold_output_dir = os.path.join(training_config.output_dir, f"fold_{current_index + 1}")
         if not os.path.exists(fold_output_dir):
             os.makedirs(fold_output_dir)
 
-        model = OB_05Model()
+        model = training_config.model_type()
         model.apply(training.init_weights)
         model.to(training.device)
 
         criterion, optimizer, scheduler = training_config.generate_hyper_parameters(model)
         patience = scheduler.patience
 
-        training_dataloader = data_loader.create_data_loader(training_subset)
-        validation_dataloader = data_loader.create_data_loader(validation_subset)
-        testing_dataloader = data_loader.create_data_loader(testing_subset)
+        training_dataloader, validation_dataloader, testing_dataloader = (
+            SubsetIndices.to_data_loaders(dataset, subset_indices))
 
         # 2. initializing values for the fold
         best_validation_loss = float("inf")
